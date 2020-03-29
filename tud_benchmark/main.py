f318dfab9d877fed79dfc44806d3bcf51ad0c058
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-03-29 13:24:01 -0400

    Added benchmark suite

--- tud_benchmark/main.py
+++ tud_benchmark/main.py
@@ -3,7 +3,7 @@ import numpy as np
 
 import pandas as pd
 from auxiliarymethods import dataset_parsers as dp
-#from auxiliarymethods import evaluation as val
+from auxiliarymethods import evaluation as val
 
 from scipy import sparse as sp
 from gnn_baselines.gin import GIN0
@@ -15,23 +15,23 @@ import auxiliarymethods.dataset_parsers as dp
 
 
 def main():
-    dataset = "MUTAG"
+    dataset = "ENZYMES"
 
 
     # all_matrices = []
-    # classes = dp.read_classes(dataset)
+    classes = dp.read_classes(dataset)
     # for i in range(0,6):
     #     gm = kb.compute_wl_1_sparse(dataset, i, True, False)
     #     gm = aux.normalize_feature_vector(gm)
     #     all_matrices.append(gm)
     # print(val.linear_svm_evaluation(all_matrices, classes, num_repetitions=10))
     #
-    # all_matrices = []
-    # for i in range(0,6):
-    #     gm = kb.compute_wl_1_dense(dataset, i, True, False)
-    #     gm = aux.normalize_gram_matrix(gm)
-    #     all_matrices.append(gm)
-    # print(val.kernel_svm_evaluation(all_matrices, classes,num_repetitions=10))
+    all_matrices = []
+    for i in range(0,6):
+        gm = kb.compute_wl_1_dense(dataset, i, True, False)
+        gm = aux.normalize_gram_matrix(gm)
+        all_matrices.append(gm)
+    print(val.kernel_svm_evaluation(all_matrices, classes,num_repetitions=10))
 
 
     print(gnn_evaluation(GIN0, dataset, [2], [64], num_repetitions=10, all_std=True))
