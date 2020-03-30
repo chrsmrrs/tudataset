89264b3eeace7ae35a16791a6734a62287d8a5d7
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-03-30 16:56:40 -0400

    Added WLOA.

--- tud_benchmark/kernel_baselines/src/AuxiliaryMethods.h
+++ tud_benchmark/kernel_baselines/src/AuxiliaryMethods.h
@@ -20,7 +20,7 @@ using Eigen::MatrixXd;
 using namespace std;
 using namespace GraphLibrary;
 
-#ifdef _LINUX
+#ifdef unix
 #include <eigen3/Eigen/Sparse>
 #include <eigen3/unsupported/Eigen/src/SparseExtra/MarketIO.h>
 #else
