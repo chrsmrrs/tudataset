53be36b2162d25d10573f1f478aa0a363b735755
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-04-06 17:53:27 -0400

    MC

--- tud_benchmark/kernel_baselines/kernel_baselines.cpp
+++ tud_benchmark/kernel_baselines/kernel_baselines.cpp
@@ -46,6 +46,10 @@ MatrixXd compute_wl_1_dense(string ds, int num_iterations,  bool use_labels,  bo
 }
 
 GramMatrix compute_wl_1_sparse_ZINC(int num_iterations,  bool use_labels,  bool use_edge_labels, const std::vector<int> &indices_train, const std::vector<int> &indices_val, const std::vector<int> &indices_test) {
+
+    GraphDatabase gdb_2 = AuxiliaryMethods::read_graph_txt_file("ZINC_val");
+    gdb.erase(gdb_2.begin() + 0);
+
    cout << "$$$" << endl;
     GraphDatabase gdb = AuxiliaryMethods::read_graph_txt_file("ZINC_train");
     gdb.erase(gdb.begin() + 0);
@@ -53,9 +57,7 @@ GramMatrix compute_wl_1_sparse_ZINC(int num_iterations,  bool use_labels,  bool 
         GraphDatabase gdb_3 = AuxiliaryMethods::read_graph_txt_file("ZINC_test");
     gdb.erase(gdb_3.begin() + 0);
    cout << "$$$" << endl;
-    GraphDatabase gdb_2 = AuxiliaryMethods::read_graph_txt_file("ZINC_val");
-    gdb.erase(gdb_2.begin() + 0);
-    cout << "$$$" << endl;
+
 
    GraphDatabase gdb_new;
    for (auto i : indices_train) {
