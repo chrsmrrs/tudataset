7ec1998ea96374345421869d54c9733f9014b4b5
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-04-08 16:41:58 -0400

    MC

--- tud_benchmark/main.py
+++ tud_benchmark/main.py
@@ -13,53 +13,87 @@ from sklearn.preprocessing import StandardScaler
 import numpy as np
 
 
-
-
 def main():
-    datataset = [["ENZYMES", True],
-                 ["IMDB-BINARY", False], ["IMDB-MULTI", False],
-                 ["NCI1", True],
-                 ["PROTEINS", True],
-                 ["REDDIT-BINARY", False],
-                 ["deezer_ego_nets", False]]
-    for d, use_labels in datataset:
-
+    datataset = [["MCF-7", True, True],
+                 ["MOLT-4", True, True],
+                 ["TRIANGLES", False, False],
+                 ["github_stargazers", False, False],
+                 ["reddit_threads", False, False]]
+    for d, use_labels, use_edge_labels in datataset:
         dataset = d
         classes = dp.get_dataset(dataset)
 
         print("WL1")
         all_matrices = []
         for i in range(1, 6):
-            gm = kb.compute_wl_1_dense(dataset, i, use_labels, False)
-            gm_n = aux.normalize_gram_matrix(gm)
+            gm = kb.compute_wl_1_sparse(dataset, i, use_labels, use_edge_labels)
+            gm_n = aux.normalize_feature_vector(gm)
             all_matrices.append(gm_n)
         print("###")
-        print(kernel_svm_evaluation(all_matrices, classes, num_repetitions=10, all_std=True))
-
-        print("WLOA")
-        all_matrices = []
-        for i in range(1, 6):
-            gm = kb.compute_wloa_dense(dataset, i, use_labels, False)
-            gm_n = aux.normalize_gram_matrix(gm)
-            all_matrices.append(gm_n)
-        print("###")
-        print(kernel_svm_evaluation(all_matrices, classes, num_repetitions=10, all_std=True))
+        print(linear_svm_evaluation(all_matrices, classes, num_repetitions=10, all_std=True, primal=True))
 
         print("GR")
         all_matrices = []
-        gm = kb.compute_graphlet_dense(dataset, use_labels, False)
-        gm_n = aux.normalize_gram_matrix(gm)
+        gm = kb.compute_graphlet_sparse(dataset, use_labels, use_edge_labels)
+        gm_n = aux.normalize_feature_vector(gm)
         all_matrices.append(gm_n)
         print("###")
-        print(kernel_svm_evaluation(all_matrices, classes, num_repetitions=10, all_std=True))
+        print(linear_svm_evaluation(all_matrices, classes, num_repetitions=10, all_std=True, primal=True))
 
         print("SP")
         all_matrices = []
-        gm = kb.compute_shortestpath_dense(dataset, use_labels)
-        gm_n = aux.normalize_gram_matrix(gm)
+        gm = kb.compute_shortestpath_sparse(dataset, use_labels)
+        gm_n = aux.normalize_feature_vector(gm)
         all_matrices.append(gm_n)
         print("###")
-        print(kernel_svm_evaluation(all_matrices, classes, num_repetitions=10, all_std=True))
+        print(linear_svm_evaluation(all_matrices, classes, num_repetitions=10, all_std=True, primal=True))
+
+
+
+    # datataset = [["ENZYMES", True],
+    #              ["IMDB-BINARY", False], ["IMDB-MULTI", False],
+    #              ["NCI1", True],
+    #              ["PROTEINS", True],
+    #              ["REDDIT-BINARY", False],
+    #              ["deezer_ego_nets", False]]
+    # for d, use_labels in datataset:
+    #
+    #     dataset = d
+    #     classes = dp.get_dataset(dataset)
+    #
+    #     print("WL1")
+    #     all_matrices = []
+    #     for i in range(1, 6):
+    #         gm = kb.compute_wl_1_dense(dataset, i, use_labels, False)
+    #         gm_n = aux.normalize_gram_matrix(gm)
+    #         all_matrices.append(gm_n)
+    #     print("###")
+    #     print(kernel_svm_evaluation(all_matrices, classes, num_repetitions=10, all_std=True))
+    #
+    #     print("WLOA")
+    #     all_matrices = []
+    #     for i in range(1, 6):
+    #         gm = kb.compute_wloa_dense(dataset, i, use_labels, False)
+    #         gm_n = aux.normalize_gram_matrix(gm)
+    #         all_matrices.append(gm_n)
+    #     print("###")
+    #     print(kernel_svm_evaluation(all_matrices, classes, num_repetitions=10, all_std=True))
+    #
+    #     print("GR")
+    #     all_matrices = []
+    #     gm = kb.compute_graphlet_dense(dataset, use_labels, False)
+    #     gm_n = aux.normalize_gram_matrix(gm)
+    #     all_matrices.append(gm_n)
+    #     print("###")
+    #     print(kernel_svm_evaluation(all_matrices, classes, num_repetitions=10, all_std=True))
+    #
+    #     print("SP")
+    #     all_matrices = []
+    #     gm = kb.compute_shortestpath_dense(dataset, use_labels)
+    #     gm_n = aux.normalize_gram_matrix(gm)
+    #     all_matrices.append(gm_n)
+    #     print("###")
+    #     print(kernel_svm_evaluation(all_matrices, classes, num_repetitions=10, all_std=True))
 
 
 if __name__ == "__main__":
