53873ee87418014ce9cd44b3572c46a147ce7b87
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-04-11 14:01:24 -0400

    dmkljdw

--- tud_benchmark/main.py
+++ tud_benchmark/main.py
@@ -50,7 +50,7 @@ def main():
     print("###")
     all_matrices = []
     for i in range(4, 5):
-        all_matrices.append(kb.compute_wl_1_sparse_ZINC(i, True, True))
+        all_matrices.append(kb.compute_wl_1_sparse_ZINC(i, True, True, indices_train, indices_val, indices_test))
         print(all_matrices[-1].shape)
     print("###")
 
