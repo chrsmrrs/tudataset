588bd981f8332c02e8e4baf51a1089b667c53144
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-04-11 15:14:57 -0400

    dmkljdw

--- tud_benchmark/main.py
+++ tud_benchmark/main.py
@@ -49,9 +49,9 @@ def main():
         all_matrices.append(kb.compute_wl_1_sparse_ZINC(i, True, True, indices_train, indices_val, indices_test))
     print("###")
 
-    indices_train = list(range(10000))
-    indices_val = list(range(1000))
-    indices_test = list(range(1000))
+    indices_train = list(range(0,10000))
+    indices_val = list(range(10000,11000))
+    indices_test = list(range(11000,12000))
     p = sgd_regressor_evaluation(all_matrices, targets, indices_train, indices_val, indices_test, num_repetitions=1,
                                  alpha=[0.0001])
     print(p)
