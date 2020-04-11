92016838fd5b27b9209a4c53601db4f4bc10786e
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-04-11 15:07:31 -0400

    dmkljdw

--- tud_benchmark/main.py
+++ tud_benchmark/main.py
@@ -43,17 +43,20 @@ def main():
     targets = np.array(targets)
     print(len(targets))
 
-    indices_train = list(range(0,10000))
-    indices_val = list(range(10000,11000))
-    indices_test = list(range(11000,12000))
-
     print("###")
-    all_matrices = [kb.compute_wl_1_sparse_ZINC(4,True, True, indices_train, indices_val, indices_test)]
+    all_matrices = []
+    for i in range(4, 5):
+        all_matrices.append(kb.compute_wl_1_sparse_ZINC(i, True, True, indices_train, indices_val, indices_test))
     print("###")
 
-    #all_matrices = [aux.normalize_feature_vector(all_matrices[-1])]
-    print("###")
-    p = ridge_regressor_evaluation(all_matrices, targets,  indices_train, indices_val, indices_test, num_repetitions=1, alpha=[0.1, 1.0])
+    indices_train = list(range(10000))
+    indices_val = list(range(1000))
+    indices_test = list(range(1000))
+    p = sgd_regressor_evaluation(all_matrices, targets, indices_train, indices_val, indices_test, num_repetitions=1,
+                                 alpha=[0.0001])
+    print(p)
+    p = ridge_regressor_evaluation(all_matrices, targets, indices_train, indices_val, indices_test, num_repetitions=1,
+                                   alpha=[1.0])
     print(p)
 
     #
