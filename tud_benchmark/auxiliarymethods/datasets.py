9ca23af257d9664d268806b4bd7891dcf097b2e0
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-04-13 22:00:21 -0400

    mf

--- tud_benchmark/auxiliarymethods/datasets.py
+++ tud_benchmark/auxiliarymethods/datasets.py
@@ -24,10 +24,21 @@ def read_targets(ds_name):
     return np.array(classes)
 
 
-def get_dataset(dataset, regression=False):
+def read_multi_targets(ds_name):
+    # Classes
+    with open("datasets/" + ds_name  + "/" + ds_name + "/raw/" + ds_name + "_graph_attributes.txt", "r") as f:
+        classes = [[float(j) for j in i.split(",")] for i in list(f)]
+    f.closed
+
+    return np.array(classes)
+
+
+def get_dataset(dataset, regression=False, multigregression=False):
     path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'datasets', dataset)
     TUDataset(path, name=dataset)
 
+    if multigregression:
+        return read_multi_targets(dataset)
     if not regression:
         return read_classes(dataset)
     else:
