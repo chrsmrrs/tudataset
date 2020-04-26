76574e800e4c11919f68ea5e66bd27a5cb2091a1
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-04-26 19:41:36 -0400

    GNN evaluation.

--- tud_benchmark/main_gnn.py
+++ tud_benchmark/main_gnn.py
@@ -13,7 +13,7 @@ import numpy as np
 
 
 def main():
-    results = gnn_evaluation(GINWithJK, "PROTEINS", [2], [64], max_num_epochs=100, batch_size=25, start_lr=0.001, num_repetitions=2,
+    results = gnn_evaluation(GINWithJK, "PROTEINS", [1,2,3,4,5], [64], max_num_epochs=100, batch_size=25, start_lr=0.001, num_repetitions=10,
                    all_std=True)
     print(results)
 
