13c08b9b4ec90e1fa86565757d9206849e9f802a
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-05-04 19:57:14 -0400

    Work on GNN evaluation code. ++

--- tud_benchmark/main_gnn.py
+++ tud_benchmark/main_gnn.py
@@ -8,10 +8,10 @@ from gnn_baselines.gnn_architectures import GIN
 def main():
 
 
-    d = "IMDB-BINARY"
+    d = "ENZYMES"
     dp.get_dataset(d)
 
-    results = gnn_evaluation(GIN, d, [2,3,4], [64], max_num_epochs=100, batch_size=128, start_lr=0.01, num_repetitions=1, all_std=True)
+    results = gnn_evaluation(GIN, d, [1,2,3,4,5], [32,64,128], max_num_epochs=100, batch_size=128, start_lr=0.01, num_repetitions=10, all_std=True)
     print(results)
 
     print(results)
