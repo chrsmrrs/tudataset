d0abdbf0eeedbe9062c85d7c3ad99063a7748561
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-05-04 15:08:05 -0400

    SVM evaluation code.

--- tud_benchmark/auxiliarymethods/kernel_evaluation.py
+++ tud_benchmark/auxiliarymethods/kernel_evaluation.py
@@ -34,7 +34,7 @@ def linear_svm_evaluation(all_feature_matrices, classes, num_repetitions=10,
                 c_test = classes[test_index]
 
                 for c in C:
-                    clf = LinearSVC(C=c, dual=False)
+                    clf = LinearSVC(C=c, dual=not primal, max_iter=max_iterations)
                     clf.fit(train, c_train)
                     val_acc = accuracy_score(c_val, clf.predict(val)) * 100.0
 
