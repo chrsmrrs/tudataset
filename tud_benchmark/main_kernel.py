2b8de52f7e8f137340e3b02cfd9029e00a4912f9
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-05-04 15:59:31 -0400

    SVM evaluation code.

--- tud_benchmark/main_kernel.py
+++ tud_benchmark/main_kernel.py
@@ -11,11 +11,11 @@ def main():
 
     all_matrices = []
     for i in range(1, 6):
-        gm = kb.compute_wl_1_sparse(dataset, i, True, False)
+        gm = kb.compute_wl_1_dense(dataset, i, True, False)
         gm_n = aux.normalize_feature_vector(gm)
         all_matrices.append(gm_n)
 
-    acc, s_1, s_2 = linear_svm_evaluation(all_matrices, classes, num_repetitions=1, all_std=True)
+    acc, s_1, s_2 = aux.normalize_gram_matrix(all_matrices, classes, num_repetitions=1, all_std=True)
     print(dataset + " " + "WL1SP " + str(acc) + " " + str(s_1) + " " + str(s_2))
 
 
