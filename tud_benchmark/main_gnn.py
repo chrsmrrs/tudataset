9570b044a51bb14709c600ca28e6f0a50067d189
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-05-04 20:03:23 -0400

    Work on GNN evaluation code. ++

--- tud_benchmark/main_gnn.py
+++ tud_benchmark/main_gnn.py
@@ -11,7 +11,7 @@ def main():
     d = "ENZYMES"
     dp.get_dataset(d)
 
-    results = gnn_evaluation(GINWithJK, d, [2,3,4,5], [64], max_num_epochs=100, batch_size=128, start_lr=0.01, num_repetitions=10, all_std=True)
+    results = gnn_evaluation(GINWithJK, d, [2,3,4], [64], max_num_epochs=100, batch_size=128, start_lr=0.01, num_repetitions=10, all_std=True)
     print(results)
 
     print(results)
