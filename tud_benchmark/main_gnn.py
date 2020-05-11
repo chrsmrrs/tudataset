ed348ce9e0b26da51e5f5d0cfcbc89a39b6475fc
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-05-11 12:38:56 -0400

    MC.

--- tud_benchmark/main_gnn.py
+++ tud_benchmark/main_gnn.py
@@ -9,7 +9,7 @@ import os.path as osp
 def main():
     num_reps = 3
 
-    # # NeuriPS stuff
+    # NeuriPS stuff
     # print("NeurIPS")
     #
     # # Smaller datasets.
@@ -32,24 +32,24 @@ def main():
     #     results.append(d + " " + "GIN " + str(acc) + " " + str(s_1) + " " + str(s_2))
     #
     # ####################################################################################################################
-    #
-    # # Larger datasets using LIBLINEAR.
-    # dataset = [["Yeast", True], ["YeastH", True], ["UACC257", True], ["UACC257H", True], ["OVCAR-8", True], ["OVCAR-8H", True]]
-    #
-    # for d, use_labels in dataset:
-    #     dp.get_dataset(d)
-    #
-    #     acc, s_1, s_2 = gnn_evaluation(GINE, d, [1, 2, 3, 4, 5], [32, 64, 128], max_num_epochs=200,
-    #                                    batch_size=128, start_lr=0.01,
-    #                                    num_repetitions=num_reps, all_std=True)
-    #     print(d + " " + "GINE " + str(acc) + " " + str(s_1) + " " + str(s_2))
-    #     results.append(d + " " + "GINE " + str(acc) + " " + str(s_1) + " " + str(s_2))
-    #
-    #     acc, s_1, s_2 = gnn_evaluation(GINE0, d, [1, 2, 3, 4, 5], [32, 64, 128], max_num_epochs=200, batch_size=128,
-    #                                    start_lr=0.01,
-    #                                    num_repetitions=num_reps, all_std=True)
-    #     print(d + " " + "GINE0 " + str(acc) + " " + str(s_1) + " " + str(s_2))
-    #     results.append(d + " " + "GINE0 " + str(acc) + " " + str(s_1) + " " + str(s_2))
+
+    # Larger datasets using LIBLINEAR.
+    dataset = [["Yeast", True], ["YeastH", True], ["UACC257", True], ["UACC257H", True], ["OVCAR-8", True], ["OVCAR-8H", True]]
+
+    for d, use_labels in dataset:
+        dp.get_dataset(d)
+
+        acc, s_1, s_2 = gnn_evaluation(GINE, d, [1, 2, 3, 4, 5], [32, 64, 128], max_num_epochs=200,
+                                       batch_size=128, start_lr=0.01,
+                                       num_repetitions=num_reps, all_std=True)
+        print(d + " " + "GINE " + str(acc) + " " + str(s_1) + " " + str(s_2))
+        results.append(d + " " + "GINE " + str(acc) + " " + str(s_1) + " " + str(s_2))
+
+        acc, s_1, s_2 = gnn_evaluation(GINE0, d, [1, 2, 3, 4, 5], [32, 64, 128], max_num_epochs=200, batch_size=128,
+                                       start_lr=0.01,
+                                       num_repetitions=num_reps, all_std=True)
+        print(d + " " + "GINE0 " + str(acc) + " " + str(s_1) + " " + str(s_2))
+        results.append(d + " " + "GINE0 " + str(acc) + " " + str(s_1) + " " + str(s_2))
 
     # exit()
     #
@@ -73,50 +73,50 @@ def main():
     #
     # ####################################################################################################################
 
-    print("TU")
-
-    # Larger datasets using LIBLINEAR.
-    dataset = [["MCF-7", True], ["MOLT-4", True]]
-
-    for d, use_labels in dataset:
-        dp.get_dataset(d)
-
-        acc, s_1, s_2 = gnn_evaluation(GINEWithJK, d, [2, 3, 4, 5], [64, 128], max_num_epochs=100,
-                                       batch_size=128, start_lr=0.01,
-                                       num_repetitions=num_reps, all_std=True)
-        print(d + " " + "GINEWithJK " + str(acc) + " " + str(s_1) + " " + str(s_2))
-        results.append(d + " " + "GINWithJK " + str(acc) + " " + str(s_1) + " " + str(s_2))
-
-        acc, s_1, s_2 = gnn_evaluation(GINE, d, [2, 3, 4, 5], [64, 128], max_num_epochs=100, batch_size=128,
-                                       start_lr=0.01,
-                                       num_repetitions=num_reps, all_std=True)
-        print(d + " " + "GINE " + str(acc) + " " + str(s_1) + " " + str(s_2))
-        results.append(d + " " + "GINE " + str(acc) + " " + str(s_1) + " " + str(s_2))
-
-
-
-    dataset = [["TRIANGLES", False],
-               ["github_stargazers", False],
-               ["reddit_threads", False]]
-
-    for d, use_labels in dataset:
-        dp.get_dataset(d)
-
-        acc, s_1, s_2 = gnn_evaluation(GINWithJK, d, [2, 3, 4, 5], [64, 128], max_num_epochs=100,
-                                       batch_size=128, start_lr=0.01,
-                                       num_repetitions=num_reps, all_std=True)
-        print(d + " " + "GINWithJK " + str(acc) + " " + str(s_1) + " " + str(s_2))
-        results.append(d + " " + "GINWithJK " + str(acc) + " " + str(s_1) + " " + str(s_2))
-
-        acc, s_1, s_2 = gnn_evaluation(GIN, d, [2, 3, 4, 5], [64, 128], max_num_epochs=100, batch_size=128,
-                                       start_lr=0.01,
-                                       num_repetitions=num_reps, all_std=True)
-        print(d + " " + "GIN " + str(acc) + " " + str(s_1) + " " + str(s_2))
-        results.append(d + " " + "GIN " + str(acc) + " " + str(s_1) + " " + str(s_2))
-
-    print("DONE! :*")
-    for r in results:
-        print(r)
+    # print("TU")
+    #
+    # # Larger datasets using LIBLINEAR.
+    # dataset = [["MCF-7", True], ["MOLT-4", True]]
+    #
+    # for d, use_labels in dataset:
+    #     dp.get_dataset(d)
+    #
+    #     acc, s_1, s_2 = gnn_evaluation(GINEWithJK, d, [2, 3, 4, 5], [64, 128], max_num_epochs=100,
+    #                                    batch_size=128, start_lr=0.01,
+    #                                    num_repetitions=num_reps, all_std=True)
+    #     print(d + " " + "GINEWithJK " + str(acc) + " " + str(s_1) + " " + str(s_2))
+    #     results.append(d + " " + "GINWithJK " + str(acc) + " " + str(s_1) + " " + str(s_2))
+    #
+    #     acc, s_1, s_2 = gnn_evaluation(GINE, d, [2, 3, 4, 5], [64, 128], max_num_epochs=100, batch_size=128,
+    #                                    start_lr=0.01,
+    #                                    num_repetitions=num_reps, all_std=True)
+    #     print(d + " " + "GINE " + str(acc) + " " + str(s_1) + " " + str(s_2))
+    #     results.append(d + " " + "GINE " + str(acc) + " " + str(s_1) + " " + str(s_2))
+    #
+    #
+    #
+    # dataset = [["TRIANGLES", False],
+    #            ["github_stargazers", False],
+    #            ["reddit_threads", False]]
+    #
+    # for d, use_labels in dataset:
+    #     dp.get_dataset(d)
+    #
+    #     acc, s_1, s_2 = gnn_evaluation(GINWithJK, d, [2, 3, 4, 5], [64, 128], max_num_epochs=100,
+    #                                    batch_size=128, start_lr=0.01,
+    #                                    num_repetitions=num_reps, all_std=True)
+    #     print(d + " " + "GINWithJK " + str(acc) + " " + str(s_1) + " " + str(s_2))
+    #     results.append(d + " " + "GINWithJK " + str(acc) + " " + str(s_1) + " " + str(s_2))
+    #
+    #     acc, s_1, s_2 = gnn_evaluation(GIN, d, [2, 3, 4, 5], [64, 128], max_num_epochs=100, batch_size=128,
+    #                                    start_lr=0.01,
+    #                                    num_repetitions=num_reps, all_std=True)
+    #     print(d + " " + "GIN " + str(acc) + " " + str(s_1) + " " + str(s_2))
+    #     results.append(d + " " + "GIN " + str(acc) + " " + str(s_1) + " " + str(s_2))
+    #
+    # print("DONE! :*")
+    # for r in results:
+    #     print(r)
 
 
 if __name__ == "__main__":
