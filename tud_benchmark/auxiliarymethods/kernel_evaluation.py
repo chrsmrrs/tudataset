a53447b4c62268d19b29bc1a6a8caca4d3c97935
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-05-13 18:03:36 -0400

    Experiments

--- tud_benchmark/auxiliarymethods/kernel_evaluation.py
+++ tud_benchmark/auxiliarymethods/kernel_evaluation.py
@@ -20,18 +20,17 @@ def linear_svm_evaluation(all_feature_matrices, classes, num_repetitions=10,
 
         for train_index, test_index in kf.split(list(range(len(classes)))):
             # Sample 10% split from training split for validation.
-            train_index, val_index = train_test_split(train_index, test_size=0.1)
+            inner_train_index, val_index = train_test_split(train_index, test_size=0.1)
             best_val_acc = 0.0
-            best_test = 0.0
+            best_gram_matrix = all_feature_matrices[0]
+            best_c = C[0]
 
             for gram_matrix in all_feature_matrices:
-                train = gram_matrix[train_index]
+                train = gram_matrix[inner_train_index]
                 val = gram_matrix[val_index]
-                test = gram_matrix[test_index]
 
                 c_train = classes[train_index]
                 c_val = classes[val_index]
-                c_test = classes[test_index]
 
                 for c in C:
                     clf = LinearSVC(C=c, tol=0.01, dual=False)
@@ -41,7 +40,18 @@ def linear_svm_evaluation(all_feature_matrices, classes, num_repetitions=10,
                     if val_acc > best_val_acc:
                         # Get test acc.
                         best_val_acc = val_acc
-                        best_test = accuracy_score(c_test, clf.predict(test)) * 100.0
+                        best_c = c
+                        best_gram_matrix = gram_matrix
+
+            # Determine test accuracy.
+            train = best_gram_matrix[train_index]
+            test = best_gram_matrix[test_index]
+
+            c_train = classes[train_index]
+            c_test = classes[test_index]
+            clf = LinearSVC(C=best_c, tol=0.01, dual=False)
+            clf.fit(train, c_train)
+            best_test = accuracy_score(c_test, clf.predict(test)) * 100.0
 
             test_accuracies.append(best_test)
             if all_std:
@@ -69,65 +79,11 @@ def kernel_svm_evaluation(all_matrices, classes, num_repetitions=10,
         kf = KFold(n_splits=10, shuffle=True)
 
         for train_index, test_index in kf.split(list(range(len(classes)))):
-            # Sample 10% split from training split for validation.
-            train_index, val_index = train_test_split(train_index, test_size=0.1)
-            best_val_acc = 0.0
-            best_test = 0.0
-
-            for gram_matrix in all_matrices:
-                train = gram_matrix[train_index, :]
-                train = train[:, train_index]
-                val = gram_matrix[val_index, :]
-                val = val[:, train_index]
-                test = gram_matrix[test_index, :]
-                test = test[:, train_index]
-
-                c_train = classes[train_index]
-                c_val = classes[val_index]
-                c_test = classes[test_index]
-
-                for c in C:
-                    clf = SVC(C=c, kernel="precomputed", tol=0.001)
-                    clf.fit(train, c_train)
-                    val_acc = accuracy_score(c_val, clf.predict(val)) * 100.0
-
-                    if val_acc > best_val_acc:
-                        # Get test acc.
-                        best_val_acc = val_acc
-                        best_test = accuracy_score(c_test, clf.predict(test)) * 100.0
-
-            test_accuracies.append(best_test)
-            if all_std:
-                test_accuracies_complete.append(best_test)
-        test_accuracies_all.append(float(np.array(test_accuracies).mean()))
-
-    if all_std:
-        return (np.array(test_accuracies_all).mean(), np.array(test_accuracies_all).std(),
-                np.array(test_accuracies_complete).std())
-    else:
-        return (np.array(test_accuracies_all).mean(), np.array(test_accuracies_all).std())
-
-
-# 10-CV for kernel svm and hyperparameter selection.
-def kernel_svm_evaluation_full(all_matrices, classes, num_repetitions=10,
-                          C=[10 ** 3, 10 ** 2, 10 ** 1, 10 ** 0, 10 ** -1, 10 ** -2, 10 ** -3], all_std=False):
-    # Acc. over all repetitions.
-    test_accuracies_all = []
-    # All acc. over all folds and repetitions.
-    test_accuracies_complete = []
-
-    for i in range(num_repetitions):
-        # Test acc. over all folds.
-        test_accuracies = []
-        kf = KFold(n_splits=10, shuffle=True)
-
-        for train_index, test_index in kf.split(list(range(len(classes)))):
-            
             # Determine hyperparameters
             inner_train_index, val_index = train_test_split(train_index, test_size=0.1)
             best_val_acc = 0.0
-            best_gram_matrix = all_matrices[0];
-            best_c = C[0];
+            best_gram_matrix = all_matrices[0]
+            best_c = C[0]
 
             for gram_matrix in all_matrices:
                 train = gram_matrix[inner_train_index, :]
@@ -147,8 +103,8 @@ def kernel_svm_evaluation_full(all_matrices, classes, num_repetitions=10,
                         best_val_acc = val_acc
                         best_c = c
                         best_gram_matrix = gram_matrix
-            
-            # Determine test accuracy
+
+            # Determine test accuracy.
             train = best_gram_matrix[train_index, :]
             train = train[:, train_index]
             test = best_gram_matrix[test_index, :]
