c4e3a711abcf7494bd7ea97b1172af8cdf36fe09
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-05-24 14:15:46 -0400

    Fixed GNN evaluation.

--- tud_benchmark/main_kernel.py
+++ tud_benchmark/main_kernel.py
@@ -30,6 +30,15 @@ def main():
 
         all_matrices = []
         for i in range(3,4):
+            gm = kb.compute_lwlp_2_sparse(dataset, i, use_labels, use_edge_labels, False)
+            gm_n = aux.normalize_feature_vector(gm)
+            all_matrices.append(gm_n)
+        acc, s_1, s_2 = linear_svm_evaluation(all_matrices, classes, num_repetitions=1, all_std=True)
+        print(d + " " + "LWLP2SP " + str(acc) + " " + str(s_1) + " " + str(s_2))
+        results.append(d + " " + "LWLP2SP " + str(acc) + " " + str(s_1) + " " + str(s_2))
+
+        all_matrices = []
+        for i in range(3,4):
             gm = kb.compute_lwl_2_sparse(dataset, i, use_labels, use_edge_labels, False)
             gm_n = aux.normalize_feature_vector(gm)
             all_matrices.append(gm_n)
@@ -37,14 +46,7 @@ def main():
         print(d + " " + "LWL2SP " + str(acc) + " " + str(s_1) + " " + str(s_2))
         results.append(d + " " + "LWL2SP " + str(acc) + " " + str(s_1) + " " + str(s_2))
 
-        all_matrices = []
-        for i in range(3,4):
-            gm = kb.compute_lwlp_2_sparse(dataset, i, use_labels, use_edge_labels, False)
-            gm_n = aux.normalize_feature_vector(gm)
-            all_matrices.append(gm_n)
-        acc, s_1, s_2 = linear_svm_evaluation(all_matrices, classes, num_repetitions=1, all_std=True)
-        print(d + " " + "LWLP2SP " + str(acc) + " " + str(s_1) + " " + str(s_2))
-        results.append(d + " " + "LWLP2SP " + str(acc) + " " + str(s_1) + " " + str(s_2))
+
 
         # all_matrices = []
         # gm = kb.compute_graphlet_sparse(dataset, use_labels, use_edge_labels)
