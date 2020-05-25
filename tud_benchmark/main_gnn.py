c2207510f407f27fc3aabea4d80b86c68795598d
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-05-24 21:09:40 -0400

    Added add pooling to GNNs.

--- tud_benchmark/main_gnn.py
+++ tud_benchmark/main_gnn.py
@@ -26,6 +26,16 @@ def main():
         print(d + " " + "GIN " + str(acc) + " " + str(s_1) + " " + str(s_2))
         results.append(d + " " + "GIN " + str(acc) + " " + str(s_1) + " " + str(s_2))
 
+        acc, s_1, s_2 = gnn_evaluation(GIN0, d, [1,2,3,4,5], [64,128], add_pool=True, max_num_epochs=200, batch_size=128,
+                                       start_lr=0.01, num_repetitions=num_reps, all_std=True)
+        print(d + " " + "GIN0 " + str(acc) + " " + str(s_1) + " " + str(s_2))
+        results.append(d + " " + "GIN0 " + str(acc) + " " + str(s_1) + " " + str(s_2))
+
+        acc, s_1, s_2 = gnn_evaluation(GIN0, d, [1,2,3,4,5], [32,64,128], add_pool=False, max_num_epochs=200, batch_size=128,
+                                       start_lr=0.01, num_repetitions=num_reps, all_std=True)
+        print(d + " " + "GIN0 " + str(acc) + " " + str(s_1) + " " + str(s_2))
+        results.append(d + " " + "GIN0 " + str(acc) + " " + str(s_1) + " " + str(s_2))
+
 
     # results = []
     # for d, use_labels in dataset:
