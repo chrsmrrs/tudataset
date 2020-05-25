8b631291b9a1dcd32a401ecb72b04090068a6b5f
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-05-24 20:15:50 -0400

    Added add pooling to GNNs.

--- tud_benchmark/gnn_baselines/gnn_architectures.py
+++ tud_benchmark/gnn_baselines/gnn_architectures.py
@@ -59,6 +59,8 @@ class GIN(torch.nn.Module):
             x = conv(x, edge_index)
 
         if self.add_pool:
+            print("######")
+            exit()
             x = global_add_pool(x, batch)
         else:
             x = global_mean_pool(x, batch)
