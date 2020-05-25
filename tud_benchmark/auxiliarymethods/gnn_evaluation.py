1897ad0a714e4dfd4e7a4ab0fef5693169f04e8f
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-05-24 22:04:37 -0400

    Added add pooling to GNNs.

--- tud_benchmark/auxiliarymethods/gnn_evaluation.py
+++ tud_benchmark/auxiliarymethods/gnn_evaluation.py
@@ -138,3 +138,89 @@ def gnn_evaluation(gnn, ds_name, layers, hidden, add_pool=False, max_num_epochs=
                 np.array(test_accuracies_complete).std())
     else:
         return (np.array(test_accuracies_all).mean(), np.array(test_accuracies_all).std())
+
+
+def gnn_evaluation_old(gnn, ds_name, layers, hidden, max_num_epochs=200, batch_size=128, start_lr=0.01, num_repetitions=10, all_std=True):
+    # Load dataset and shuffle.
+    path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'datasets', ds_name)
+    dataset = TUDataset(path, name=ds_name).shuffle()
+
+    # One-hot degree if node labels are not available.
+    if dataset.data.x is None:
+        max_degree = 0
+        degs = []
+        for data in dataset:
+            degs += [degree(data.edge_index[0], dtype=torch.long)]
+            max_degree = max(max_degree, degs[-1].max().item())
+
+        if max_degree < 1000:
+            dataset.transform = T.OneHotDegree(max_degree)
+        else:
+            deg = torch.cat(degs, dim=0).to(torch.float)
+            mean, std = deg.mean().item(), deg.std().item()
+            dataset.transform = NormalizedDegree(mean, std)
+
+    # Set device.
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    test_accuracies_all = []
+    test_accuracies_complete = []
+
+    for i in range(num_repetitions):
+        # Test acc. over all folds.
+        test_accuracies = []
+        kf = KFold(n_splits=10, shuffle=True)
+        dataset.shuffle()
+
+        for train_index, test_index in kf.split(list(range(len(dataset)))):
+            # Sample 10% split from training split for validation.
+            train_index, val_index = train_test_split(train_index, test_size=0.1)
+            best_val_acc = 0.0
+            best_test = 0.0
+
+            # Split data.
+            train_dataset = dataset[train_index.tolist()]
+            val_dataset = dataset[val_index.tolist()]
+            test_dataset = dataset[test_index.tolist()]
+
+            # Prepare batching.
+            train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+            val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
+            test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+
+            # Collect val. and test acc. over all hyperparameter combinations.
+            for l in layers:
+                for h in hidden:
+                    # Setup model.
+                    model = gnn(dataset, l, h).to(device)
+                    model.reset_parameters()
+
+                    optimizer = torch.optim.Adam(model.parameters(), lr=start_lr)
+                    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min',
+                                                                           factor=0.5, patience=5,
+                                                                           min_lr=0.0000001)
+                    for epoch in range(1, max_num_epochs + 1):
+                        lr = scheduler.optimizer.param_groups[0]['lr']
+                        train(train_loader, model, optimizer, device)
+                        val_acc = test(val_loader, model, device)
+                        scheduler.step(val_acc)
+
+                        if val_acc > best_val_acc:
+                            best_val_acc = val_acc
+                            best_test = test(test_loader, model, device)*100.0
+
+                        # Break if learning rate is smaller 10**-6.
+                        if lr < 0.000001:
+                            break
+
+            test_accuracies.append(best_test)
+
+            if all_std:
+                test_accuracies_complete.append(best_test)
+        test_accuracies_all.append(float(np.array(test_accuracies).mean()))
+
+    if all_std:
+        return (np.array(test_accuracies_all).mean(), np.array(test_accuracies_all).std(),
+                np.array(test_accuracies_complete).std())
+    else:
+        return (np.array(test_accuracies_all).mean(), np.array(test_accuracies_all).std())
