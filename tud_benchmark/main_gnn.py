f925b90657c3c542d6772621d09f5bf3530b769f
Christopher Morris <christopher.morris@udo.edu>
2020-06-15 22:53:07 -0400

    Cleanup.

--- tud_benchmark/main_gnn.py
+++ tud_benchmark/main_gnn.py
@@ -39,11 +39,11 @@ def main():
         print(d + " " + "GINE " + str(acc) + " " + str(s_1) + " " + str(s_2))
         results.append(d + " " + "GINE " + str(acc) + " " + str(s_1) + " " + str(s_2))
 
-        acc, s_1, s_2 = gnn_evaluation(GINE0, d, [1, 2, 3, 4, 5], [32, 64, 128], max_num_epochs=200, batch_size=64,
+        acc, s_1, s_2 = gnn_evaluation(GINEWithJK, d, [1, 2, 3, 4, 5], [32, 64, 128], max_num_epochs=200, batch_size=64,
                                        start_lr=0.01,
                                        num_repetitions=num_reps, all_std=True)
-        print(d + " " + "GINE0 " + str(acc) + " " + str(s_1) + " " + str(s_2))
-        results.append(d + " " + "GINE0 " + str(acc) + " " + str(s_1) + " " + str(s_2))
+        print(d + " " + "GINEJK " + str(acc) + " " + str(s_1) + " " + str(s_2))
+        results.append(d + " " + "GINEJK " + str(acc) + " " + str(s_1) + " " + str(s_2))
 
 
 
