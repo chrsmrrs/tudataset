5041121c1321fe680c35330ec1bd73e192bad7c2
Christopher Morris <christopher.morris@udo.edu>
2020-06-27 17:18:14 -0400

    Cleanup.

--- tud_benchmark/main_gnn.py
+++ tud_benchmark/main_gnn.py
@@ -1,5 +1,4 @@
 import auxiliarymethods.datasets as dp
-import os.path as osp
 from auxiliarymethods.gnn_evaluation import gnn_evaluation, gnn_evaluation_old
 from gnn_baselines.gnn_architectures import GIN, GINWithJK, GINE, GINEWithJK
 
@@ -7,43 +6,52 @@ from gnn_baselines.gnn_architectures import GIN, GINWithJK, GINE, GINEWithJK
 def main():
     num_reps = 10
 
-    # Smaller datasets.
+    ### Smaller datasets.
     dataset = [["IMDB-BINARY", False], ["IMDB-MULTI", False], ["NCI1", True], ["PROTEINS", True],
-                ["REDDIT-BINARY", False], ["ENZYMES", True]]
+               ["REDDIT-BINARY", False], ["ENZYMES", True]]
 
     results = []
-    # for d, use_labels in dataset:
-    #     dp.get_dataset(d)
-    #
-    #     acc, s_1, s_2 = gnn_evaluation(GIN, d, [1, 2, 3, 4, 5], [32, 64, 128], max_num_epochs=200, batch_size=64,
-    #                                 start_lr=0.01, num_repetitions=num_reps, all_std=True)
-    #     print(d + " " + "GIN " + str(acc) + " " + str(s_1) + " " + str(s_2))
-    #     results.append(d + " " + "GIN " + str(acc) + " " + str(s_1) + " " + str(s_2))
-    #
-    #     acc, s_1, s_2 = gnn_evaluation_old(GINWithJK, d, [1, 2, 3, 4, 5], [32, 64, 128], max_num_epochs=200, batch_size=64,
-    #                                    start_lr=0.01, num_repetitions=num_reps, all_std=True)
-    #     print(d + " " + "GINWithJK " + str(acc) + " " + str(s_1) + " " + str(s_2))
-    #     results.append(d + " " + "GINWithJK " + str(acc) + " " + str(s_1) + " " + str(s_2))
+    for d, use_labels in dataset:
+        # Download dataset.
+        dp.get_dataset(d)
 
-    # Midscale datasets.
+        # GIN, datasets d, layers in [1:5], hidden dimension in {32,64,128}.
+        acc, s_1, s_2 = gnn_evaluation(GIN, d, [1, 2, 3, 4, 5], [32, 64, 128], max_num_epochs=200, batch_size=64,
+                                       start_lr=0.01, num_repetitions=num_reps, all_std=True)
+        print(d + " " + "GIN " + str(acc) + " " + str(s_1) + " " + str(s_2))
+        results.append(d + " " + "GIN " + str(acc) + " " + str(s_1) + " " + str(s_2))
+
+        # GIN with jumping knowledge, datasets d, layers in [1:5], hidden dimension in {32,64,128}.
+        acc, s_1, s_2 = gnn_evaluation_old(GINWithJK, d, [1, 2, 3, 4, 5], [32, 64, 128], max_num_epochs=200,
+                                           batch_size=64,
+                                           start_lr=0.01, num_repetitions=num_reps, all_std=True)
+        print(d + " " + "GINWithJK " + str(acc) + " " + str(s_1) + " " + str(s_2))
+        results.append(d + " " + "GINWithJK " + str(acc) + " " + str(s_1) + " " + str(s_2))
+
+    ### Midscale datasets.
     dataset = [["Yeast", True], ["YeastH", True], ["UACC257", True], ["UACC257H", True], ["OVCAR-8", True],
                ["OVCAR-8H", True]]
 
     for d, use_labels in dataset:
         dp.get_dataset(d)
 
+        # GINE (GIN with edge labels), datasets d, layers in [1:5], hidden dimension in {32,64,128}.
         acc, s_1, s_2 = gnn_evaluation_old(GINE, d, [1, 2, 3, 4, 5], [32, 64, 128], max_num_epochs=200,
-                                       batch_size=64, start_lr=0.01,
-                                       num_repetitions=num_reps, all_std=True)
+                                           batch_size=64, start_lr=0.01,
+                                           num_repetitions=num_reps, all_std=True)
         print(d + " " + "GINE " + str(acc) + " " + str(s_1) + " " + str(s_2))
         results.append(d + " " + "GINE " + str(acc) + " " + str(s_1) + " " + str(s_2))
 
-        acc, s_1, s_2 = gnn_evaluation_old(GINEWithJK, d, [1, 2, 3, 4, 5], [32, 64, 128], max_num_epochs=200, batch_size=64,
-                                       start_lr=0.01,
-                                       num_repetitions=num_reps, all_std=True)
+        # GINE (GIN with edge labels) with jumping knowledge, datasets d, layers in [1:5], hidden dimension in {32,64,128}.
+        acc, s_1, s_2 = gnn_evaluation_old(GINEWithJK, d, [1, 2, 3, 4, 5], [32, 64, 128], max_num_epochs=200,
+                                           batch_size=64,
+                                           start_lr=0.01,
+                                           num_repetitions=num_reps, all_std=True)
         print(d + " " + "GINEJK " + str(acc) + " " + str(s_1) + " " + str(s_2))
         results.append(d + " " + "GINEJK " + str(acc) + " " + str(s_1) + " " + str(s_2))
 
+        for r in results:
+            print(r)
 
 
 if __name__ == "__main__":
--- tud_benchmark/main_kernel.py
+++ tud_benchmark/main_kernel.py
@@ -1,73 +1,71 @@
 import auxiliarymethods.auxiliary_methods as aux
 import auxiliarymethods.datasets as dp
 import kernel_baselines as kb
-from auxiliarymethods.kernel_evaluation import linear_svm_evaluation
 from auxiliarymethods.kernel_evaluation import kernel_svm_evaluation
+from auxiliarymethods.kernel_evaluation import linear_svm_evaluation
 
 
 def main():
-    # Smaller datasets using LIBSVM.
+    ### Smaller datasets using LIBSVM.
     dataset = [["ENZYMES", True], ["IMDB-BINARY", False], ["IMDB-MULTI", False], ["NCI1", True], ["PROTEINS", True],
-                 ["REDDIT-BINARY", False]]
+               ["REDDIT-BINARY", False]]
+
+    # Number of repetitions of 10-CV.
+    num_reps = 10
 
     results = []
 
-    # for d, use_labels in dataset:
-    #     dataset = d
-    #     classes = dp.get_dataset(dataset)
-    #
-    #     all_matrices = []
-    #     for i in range(1, 6):
-    #         gm = kb.compute_wl_1_dense(dataset, i, use_labels, False)
-    #         gm_n = aux.normalize_gram_matrix(gm)
-    #         all_matrices.append(gm_n)
-    #     acc, s_1, s_2 = kernel_svm_evaluation(all_matrices, classes, num_repetitions=10, all_std=True)
-    #     print(d + " " + "WL1 " + str(acc) + " " + str(s_1) + " " + str(s_2))
-    #     results.append(d + " " + "WL1 " + str(acc) + " " + str(s_1) + " " + str(s_2))
-    #
-    #     all_matrices = []
-    #     for i in range(1, 6):
-    #         gm = kb.compute_wloa_dense(dataset, i, use_labels, False)
-    #         gm_n = aux.normalize_gram_matrix(gm)
-    #         all_matrices.append(gm_n)
-    #     acc, s_1, s_2 = kernel_svm_evaluation(all_matrices, classes, num_repetitions=10, all_std=True)
-    #     print(d + " " + "WLOA " + str(acc) + " " + str(s_1) + " " + str(s_2))
-    #     results.append(d + " " + "WLOA " + str(acc) + " " + str(s_1) + " " + str(s_2))
-    #
-    #     all_matrices = []
-    #     gm = kb.compute_graphlet_dense(dataset, use_labels, False)
-    #     gm_n = aux.normalize_gram_matrix(gm)
-    #     all_matrices.append(gm_n)
-    #     acc, s_1, s_2 = kernel_svm_evaluation(all_matrices, classes, num_repetitions=10, all_std=True)
-    #     print(d + " " + "GR " + str(acc) + " " + str(s_1) + " " + str(s_2))
-    #     results.append(d + " " + "GR " + str(acc) + " " + str(s_1) + " " + str(s_2))
-    #
-    #     all_matrices = []
-    #     gm = kb.compute_shortestpath_dense(dataset, use_labels)
-    #     gm_n = aux.normalize_gram_matrix(gm)
-    #     all_matrices.append(gm_n)
-    #     acc, s_1, s_2 = kernel_svm_evaluation(all_matrices, classes, num_repetitions=10, all_std=True)
-    #     print(d + " " + "SP " + str(acc) + " " + str(s_1) + " " + str(s_2))
-    #     results.append(d + " " + "SP " + str(acc) + " " + str(s_1) + " " + str(s_2))
-    #
-    # ####################################################################################################################
+    for dataset, use_labels in dataset:
+        classes = dp.get_dataset(dataset)
 
-    # Larger datasets using LIBLINEAR.
-    num_reps= 10
+        # 1-WL kernel, number of iterations in [1:6].
+        all_matrices = []
+        for i in range(1, 6):
+            gm = kb.compute_wl_1_dense(dataset, i, use_labels, False)
+            gm_n = aux.normalize_gram_matrix(gm)
+            all_matrices.append(gm_n)
+        acc, s_1, s_2 = kernel_svm_evaluation(all_matrices, classes, num_repetitions=10, all_std=True)
+        print(dataset + " " + "WL1 " + str(acc) + " " + str(s_1) + " " + str(s_2))
+        results.append(dataset + " " + "WL1 " + str(acc) + " " + str(s_1) + " " + str(s_2))
 
-    # dataset = [["MOLT-4", True, True], ["Yeast", False, False], ["MCF-7", True, True],
-    #              ["github_stargazers", False, False],
-    #              ["reddit_threads", False, False]]
+        # WLOA kernel, number of iterations in [1:6].
+        all_matrices = []
+        for i in range(1, 6):
+            gm = kb.compute_wloa_dense(dataset, i, use_labels, False)
+            gm_n = aux.normalize_gram_matrix(gm)
+            all_matrices.append(gm_n)
+        acc, s_1, s_2 = kernel_svm_evaluation(all_matrices, classes, num_repetitions=10, all_std=True)
+        print(dataset + " " + "WLOA " + str(acc) + " " + str(s_1) + " " + str(s_2))
+        results.append(dataset + " " + "WLOA " + str(acc) + " " + str(s_1) + " " + str(s_2))
 
-    dataset = [["Yeast", True, True], ["MCF-7", True, True],
+        # Graphlet kernel.
+        all_matrices = []
+        gm = kb.compute_graphlet_dense(dataset, use_labels, False)
+        gm_n = aux.normalize_gram_matrix(gm)
+        all_matrices.append(gm_n)
+        acc, s_1, s_2 = kernel_svm_evaluation(all_matrices, classes, num_repetitions=10, all_std=True)
+        print(dataset + " " + "GR " + str(acc) + " " + str(s_1) + " " + str(s_2))
+        results.append(dataset + " " + "GR " + str(acc) + " " + str(s_1) + " " + str(s_2))
+
+        # Shortest-path kernel.
+        all_matrices = []
+        gm = kb.compute_shortestpath_dense(dataset, use_labels)
+        gm_n = aux.normalize_gram_matrix(gm)
+        all_matrices.append(gm_n)
+        acc, s_1, s_2 = kernel_svm_evaluation(all_matrices, classes, num_repetitions=10, all_std=True)
+        print(dataset + " " + "SP " + str(acc) + " " + str(s_1) + " " + str(s_2))
+        results.append(dataset + " " + "SP " + str(acc) + " " + str(s_1) + " " + str(s_2))
+
+    ### Larger datasets using LIBLINEAR with edge labels.
+    dataset = [["MOLT-4", True, True], ["Yeast", True, True], ["MCF-7", True, True],
                  ["github_stargazers", False, False],
                  ["reddit_threads", False, False]]
 
     for d, use_labels, use_edge_labels in dataset:
-        print(d)
         dataset = d
         classes = dp.get_dataset(dataset)
 
+        # 1-WL kernel, number of iterations in [1:6].
         all_matrices = []
         for i in range(1, 6):
             gm = kb.compute_wl_1_sparse(dataset, i, use_labels, use_edge_labels)
@@ -78,6 +76,7 @@ def main():
         print(d + " " + "WL1SP " + str(acc) + " " + str(s_1) + " " + str(s_2))
         results.append(d + " " + "WL1SP " + str(acc) + " " + str(s_1) + " " + str(s_2))
 
+        # Graphlet kernel, number of iterations in [1:6].
         all_matrices = []
         gm = kb.compute_graphlet_sparse(dataset, use_labels, use_edge_labels)
         gm_n = aux.normalize_feature_vector(gm)
@@ -87,6 +86,7 @@ def main():
         print(d + " " + "GRSP " + str(acc) + " " + str(s_1) + " " + str(s_2))
         results.append(d + " " + "GRSP " + str(acc) + " " + str(s_1) + " " + str(s_2))
 
+        # Shortest-path kernel.
         all_matrices = []
         gm = kb.compute_shortestpath_sparse(dataset, use_labels)
         gm_n = aux.normalize_feature_vector(gm)
@@ -96,8 +96,6 @@ def main():
         print(d + " " + "SPSP " + str(acc) + " " + str(s_1) + " " + str(s_2))
         results.append(d + " " + "SPSP " + str(acc) + " " + str(s_1) + " " + str(s_2))
 
-
-    print("DONE! :*")
     for r in results:
         print(r)
 
