00e28fd9bea550b00121300803102f8f60f8cc13
Christopher Morris <christopher.morris@udo.edu>
2020-06-29 21:24:49 -0400

    Cleanup.

--- tud_benchmark/main_gnn.py
+++ tud_benchmark/main_gnn.py
@@ -11,22 +11,25 @@ def main():
                ["REDDIT-BINARY", False], ["ENZYMES", True]]
 
     results = []
-    for d, use_labels in dataset:
-        # Download dataset.
-        dp.get_dataset(d)
+    # for d, use_labels in dataset:
+    #     # Download dataset.
+    #     dp.get_dataset(d)
+    #
+    #     # GIN, dataset d, layers in [1:5], hidden dimension in {32,64,128}.
+    #     acc, s_1, s_2 = gnn_evaluation(GIN, d, [1, 2, 3, 4, 5], [32, 64, 128], max_num_epochs=200, batch_size=64,
+    #                                    start_lr=0.01, num_repetitions=num_reps, all_std=True)
+    #     print(d + " " + "GIN " + str(acc) + " " + str(s_1) + " " + str(s_2))
+    #     results.append(d + " " + "GIN " + str(acc) + " " + str(s_1) + " " + str(s_2))
+    #
+    #     # GIN with jumping knowledge, dataset d, layers in [1:5], hidden dimension in {32,64,128}.
+    #     acc, s_1, s_2 = gnn_evaluation(GINWithJK, d, [1, 2, 3, 4, 5], [32, 64, 128], max_num_epochs=200,
+    #                                        batch_size=64,
+    #                                        start_lr=0.01, num_repetitions=num_reps, all_std=True)
+    #     print(d + " " + "GINWithJK " + str(acc) + " " + str(s_1) + " " + str(s_2))
+    #     results.append(d + " " + "GINWithJK " + str(acc) + " " + str(s_1) + " " + str(s_2))
 
-        # GIN, dataset d, layers in [1:5], hidden dimension in {32,64,128}.
-        acc, s_1, s_2 = gnn_evaluation(GIN, d, [1, 2, 3, 4, 5], [32, 64, 128], max_num_epochs=200, batch_size=64,
-                                       start_lr=0.01, num_repetitions=num_reps, all_std=True)
-        print(d + " " + "GIN " + str(acc) + " " + str(s_1) + " " + str(s_2))
-        results.append(d + " " + "GIN " + str(acc) + " " + str(s_1) + " " + str(s_2))
 
-        # GIN with jumping knowledge, dataset d, layers in [1:5], hidden dimension in {32,64,128}.
-        acc, s_1, s_2 = gnn_evaluation(GINWithJK, d, [1, 2, 3, 4, 5], [32, 64, 128], max_num_epochs=200,
-                                           batch_size=64,
-                                           start_lr=0.01, num_repetitions=num_reps, all_std=True)
-        print(d + " " + "GINWithJK " + str(acc) + " " + str(s_1) + " " + str(s_2))
-        results.append(d + " " + "GINWithJK " + str(acc) + " " + str(s_1) + " " + str(s_2))
+    num = 3
 
     ### Midscale datasets.
     dataset = [["Yeast", True], ["YeastH", True], ["UACC257", True], ["UACC257H", True], ["OVCAR-8", True],
